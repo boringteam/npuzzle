b29dfc748917c31dd63d5913d63e64af6ebb4373
Audrey Roemer <audreyroemer@gmail.com>
2019-06-01 18:51:45 +0200

    add tests for manhattan and getindexof

---
+++ algo/heuristic_test.go
@@ -0,0 +1,32 @@
+package algo
+
+import (
+	"testing"
+  "npuzzle/checker"
+)
+
+func TestManhattan(t *testing.T) {
+  var res []int
+  var dist int
+	a := []int{3, 5, 1, 8, 0, 4, 2, 6, 7}
+	res = checker.BuildCorrectResult(3)
+  dist = CalculateManhattanDistance(a, res)
+  if dist != 12 {
+        t.Errorf("Error: Manhattan Distance should be 12 and not %d", dist)
+    }
+  b := []int{8, 2, 7, 4, 1, 0, 10, 15, 3, 9, 6, 13, 11, 5, 12, 14}
+  res = checker.BuildCorrectResult(4)
+  dist = CalculateManhattanDistance(b, res)
+  if dist != 38 {
+        t.Errorf("Error: Manhattan Distance should be 38 and not %d", dist)
+    }
+}
+
+func TestGetIndexOf(t *testing.T) {
+  a := []int{3, 5, 1, 8, 0, 4, 2, 6, 7}
+  b := 4
+  index := GetIndexOf(b, a)
+  if index != 5 {
+    t.Errorf("Error: index should be 5 and not %d", index)
+  }
+}
