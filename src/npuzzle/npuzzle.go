309304de09674b56dffb7192afb871f9653a532e
sjimenez <sjimenez@vente-privee.com>
2019-07-14 19:55:19 +0200

    Working

---
+++ src/npuzzle/npuzzle.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"algo"
+	"checker"
+	"utils"
+	"os"
+)
+
+func main() {
+	tab, heuristic, visual := GenerateNPuzzle()
+	utils.InitUtils(tab)
+	if visual == false {
+		utils.PrintTab(tab)
+	}
+	result := checker.BuildCorrectResult(utils.Size)
+	isSolvable := checker.CheckSolvable(tab, result, utils.Size)
+	if !isSolvable {
+		fmt.Fprintf(os.Stderr, "This puzzle is not solvable\n")
+		os.Exit(1)
+	}
+	algo.AStar(tab, result, heuristic, visual)
+}
